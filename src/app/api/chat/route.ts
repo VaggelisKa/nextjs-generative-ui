3efaf77e03bdfdcd1d380e169f3f5d925c2d8e90
VaggelisKa <e.karavasileiadis@gmail.com>
2024-09-16 07:28:13 +0200

    Create project skeleton

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -1,20 +1,3 @@
-import { createAmazonBedrock } from "@ai-sdk/amazon-bedrock";
-import { CoreMessage, streamText } from "ai";
-
-let bedrock = createAmazonBedrock({
-  region: "eu-central-1",
-  accessKeyId: "AKIAV2TFLH22PJILH7OS",
-  secretAccessKey: "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",
-});
-
 export async function POST(req: Request) {
-  const { messages }: { messages: CoreMessage[] } = await req.json();
-
-  const result = await streamText({
-    model: bedrock("anthropic.claude-v2:1"),
-    system: "You are a helpful assistant.",
-    messages,
-  });
-
-  return result.toDataStreamResponse();
+  return new Response("Hello, world!");
 }
--- src/app/gen-ai-chat/page.tsx
+++ src/app/gen-ai-chat/page.tsx
@@ -1,26 +1,5 @@
 "use client";
 
-import { useChat } from "ai/react";
-import { Chat, MessageBox } from "~/components/Chat";
-
 export default function GenAiChatPage() {
-  const { messages, input, setInput, append } = useChat();
-
-  const handleSubmit = async (event: any) => {
-    event.preventDefault();
-
-    append({ role: "user", content: input });
-  };
-
-  return (
-    <Chat onUserInputChange={setInput} onUserMessageSubmit={handleSubmit}>
-      <MessageBox from="assistant">Hello, how can I help you?</MessageBox>
-
-      {messages.map((message: any) => (
-        <MessageBox key={message.id} from={message.role}>
-          {message.content}
-        </MessageBox>
-      ))}
-    </Chat>
-  );
+  return <div>Gen AI Chat</div>;
 }
