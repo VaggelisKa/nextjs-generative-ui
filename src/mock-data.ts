c2cd5749827407df3c23f4f1f78eee89ee587550
VaggelisKa <e.karavasileiadis@gmail.com>
2024-09-23 21:02:38 +0200

    Include accounts summary tool

--- src/mock-data.ts
+++ src/mock-data.ts
@@ -1,6 +1,67 @@
 import { generateId } from "ai";
 import { format, subDays } from "date-fns";
 
+const ACCOUNTS = [
+  {
+    id: generateId(),
+    name: "Checking Account",
+    accountType: "checking",
+    balance: 1000,
+  },
+  {
+    id: generateId(),
+    name: "ASK Account",
+    accountType: "savings",
+    balance: 123000,
+  },
+  {
+    id: generateId(),
+    name: "Normal Deposit",
+    accountType: "savings",
+    balance: 5000,
+  },
+  {
+    id: generateId(),
+    name: "My favorite account",
+    accountType: "credit",
+    balance: 10000,
+  },
+];
+
+export async function getAccounts({
+  type,
+  name,
+}: {
+  type?: string;
+  name?: string;
+}) {
+  return ACCOUNTS.filter(
+    (account) =>
+      (type && account.accountType === type) ||
+      (name && account.name.toLowerCase().includes(name?.toLowerCase())),
+  );
+}
+
+export async function getAccountsSummary({
+  type,
+  name,
+}: {
+  type?: string;
+  name?: string;
+}) {
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+  let accounts = await getAccounts({ type, name });
+
+  return accounts.reduce(
+    (acc, account) => {
+      acc[account.accountType] =
+        (acc[account.accountType] || 0) + account.balance;
+      return acc;
+    },
+    {} as Record<string, number>,
+  );
+}
+
 export function getMockTimeseriesData(fromDate?: string) {
   let today = new Date();
   let mockTimeseries = [];
@@ -12,16 +73,14 @@ export function getMockTimeseriesData(fromDate?: string) {
     });
   }
 
-  console.log("From date", fromDate);
-
   if (fromDate) {
     let fromDateIndex = mockTimeseries.findIndex(
-      (item) => format(item.timestamp, "yyyy-MM-dd") === fromDate
+      (item) => format(item.timestamp, "yyyy-MM-dd") === fromDate,
     );
 
     mockTimeseries = mockTimeseries.slice(
       fromDateIndex + 1,
-      mockTimeseries.length
+      mockTimeseries.length,
     );
   }
 
@@ -50,12 +109,12 @@ export function getMockPaymentTransactions(fromDate?: string) {
 
   if (fromDate) {
     let fromDateIndex = mockTransactions.findIndex(
-      (item) => format(item.timestamp, "yyyy-MM-dd") === fromDate
+      (item) => format(item.timestamp, "yyyy-MM-dd") === fromDate,
     );
 
     mockTransactions = mockTransactions.slice(
       fromDateIndex + 1,
-      mockTransactions.length
+      mockTransactions.length,
     );
   }
 
