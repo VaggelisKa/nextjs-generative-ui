8e5fac98c9a1bd17d406eeefb8a4c466d74aa344
VaggelisKa <e.karavasileiadis@gmail.com>
2024-09-28 15:45:09 +0200

    Add session token

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -3,8 +3,10 @@ import { CoreMessage, streamText } from "ai";
 
 let bedrock = createAmazonBedrock({
   region: "eu-central-1",
-  accessKeyId: "AKIAV2TFLH22HRXD2BIL",
-  secretAccessKey: "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",
+  accessKeyId: "ASIAV2TFLH22NBSRYUQJ",
+  secretAccessKey: "YEMouEqTigVZRlZ765gV5tlVtORujvgQMVPy2Las",
+  sessionToken:
+    "IQoJb3JpZ2luX2VjEP7//////////wEaDGV1LWNlbnRyYWwtMSJHMEUCICGTRKvjQbg41ZBNXgexfj1e0H/noc6xdAwR5y3+iCqQAiEAkoXWg9E/OWuXGrGuXB7iKDlFECvY7SJi/a2bBXvf0j0q6wEIRxABGgw0MDA3MTgyNDk2NTIiDNLculvgkBeFIL4p6SrIAUM1TSiQhRBep1nniYilVoennDMOnjDOc0VZebVqVQ/SdS3MZeAKZWR/glGltk/PPQ8hVSxvMl77x8hJsSrJnbofIF6q8iz1VQiv9zkeYHu799aq99B2RanzFvA9rDimdVu56aIYnT1m4Vh1Qe1aci21qFN7blrIIbt0e/cSWGeqsGivpEW1iBbJnPCHYL69FExB7FP32lunCMUcnRH8aLHUJtb0+Z7UNJy7qBYsQSOSF7M/K7InHl6Qbibgf3yyv/JzWp7FBBhiMLyP4LcGOpgBI+q/kFY2EuvMXNvxyQMKDm8tQDprSETdstjlkgER2r7CPMpWW6+T1AlBeig3jIjtjNHwACYqqUh+QIq9pcsfnUGdX3ayudlXKupyOop7tTCX18UU8kiHouEL89cBG2z+KB2gVywYW3zpBf9PY5r9p7b4wRUCCm8hHkA0Eiqq+3C22pKvdtn8ZcZqKNiFMa2i+mJkWASi98U=",
 });
 
 export async function POST(req: Request) {
