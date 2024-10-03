e2609eddd40c5e2fb9a8fe7d86b8b8e8a22d74b6
Vaggelis Karavasileiadis <e.karavasileiadis@gmail.com>
2024-10-03 21:01:14 +0200

    Provide credentials in bedrock options

--- src/lib/bedrock.ts
+++ src/lib/bedrock.ts
@@ -4,6 +4,10 @@ import { createAmazonBedrock } from "@ai-sdk/amazon-bedrock";
 
 export const bedrock = createAmazonBedrock({
   region: "eu-central-1",
-  accessKeyId: process.env.BEDROCK_ACCESS_KEY_ID,
-  secretAccessKey: process.env.BEDROCK_SECRET_ACCESS_KEY,
+  bedrockOptions: {
+    credentials: {
+      accessKeyId: process.env.BEDROCK_ACCESS_KEY_ID!,
+      secretAccessKey: process.env.BEDROCK_SECRET_ACCESS_KEY!,
+    },
+  },
 });
